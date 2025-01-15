a08171e83a605c893f76b95d936bf04fb93c5d05
Riley Sweeting <rileyts2004@gmail.com>
2025-01-15 14:38:35 -0500

    Added both if-else programs

---
+++ odd_even.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+    // Variables
+    int num;
+
+    // Read input from user
+    printf("ENTER NUMBER: ");
+
+    // Scan number
+    scanf("%d", &num);
+
+    // Display results
+    if (num % 2 == 0) {
+        printf('The number %d is even. \n', num);
+    } else {
+        printf('The number %d is odd. \n', num);
+    }
+
+    // Return
+    return 0;
+}
---
+++ positive_negative.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main() {
+    // Variables
+    int num;
+
+    // Read input from user
+    printf("ENTER NUMBER: ");
+
+    // Scan number
+    scanf("%d", &num);
+
+    // Display results
+    if (num > 0) {
+        printf('The number %d is positive. \n', num);
+    } else if (num == 0) {
+        printf('The number %d is 0. \n', num);
+    } else {
+        printf('The number %d is negative. \n', num);
+    }
+
+    // Return
+    return 0;
+}
